3e93a32ce5a94b94d7546364fb2306c5e4698b4b
Said-MZ <saidzain897@gmail.com>
2026-02-03 21:14:09 +0300

    fuck this project

--- db/migrate/20260131162104_create_cards.rb
+++
@@ -1,9 +0,0 @@
-class CreateCards < ActiveRecord::Migration[8.1]
-  def change
-    create_table :cards do |t|
-      t.string :name
-
-      t.timestamps
-    end
-  end
-end
--- db/migrate/20260202173753_add_front_and_back_to_cards.rb
+++
@@ -1,6 +0,0 @@
-class AddFrontAndBackToCards < ActiveRecord::Migration[8.1]
-  def change
-    add_column :cards, :front, :text
-    add_column :cards, :back, :text
-  end
-end
